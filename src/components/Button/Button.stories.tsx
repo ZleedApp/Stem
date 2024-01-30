70718ebde77d729c705de9ba40839be074cbf571
TheClashFruit <clash.fruit123@gmail.com>
2024-01-30 19:50:40 +0100

    feat: button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,26 +1,43 @@
 import type {Meta, StoryObj} from '@storybook/react';
 
+import {Info, Command} from 'lucide-react';
+
 import Button from './Button';
 
 const meta: Meta<typeof Button> = {
   component: Button,
   tags: ['autodocs'],
   argTypes: {
-    label: { control: 'label' },
+    label: { control: 'text' },
+    outlined: { control: 'boolean' },
+    icon: { control: 'function' },
+    color: { control: 'select' },
   },
+  args: {
+    label: 'Button',
+    outlined: false,
+    icon: Info,
+    color: 'primary'
+  }
 };
 
-export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Regular: Story = {
   args: {
     label: 'Button',
+    icon: Info,
+    color: 'primary'
   },
 };
 
 export const Outlined: Story = {
   args: {
     label: 'Button',
+    outlined: true,
+    icon: Info,
+    color: 'red'
   },
 };
+
+export default meta;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 
 import './Button.scss';
 
+import {
+  Command,
+  LucideIcon
+} from 'lucide-react';
+
 export interface ButtonProps {
   label: string;
+  outlined: boolean;
+  icon: LucideIcon;
+  color: 'primary' | 'red' | 'yellow' | 'green' | 'blue';
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <button className={props.outlined ? 'btn outlined' : 'btn'} data-color={props.color}>
+      <span className="text">{props.label}</span>
+      {props.icon ? <props.icon className="icon" size={16} /> : <Command className="icon" size={16} />}
+    </button>
+  );
 };
 
 export default Button;
