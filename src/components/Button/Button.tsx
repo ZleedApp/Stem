0f12204db4b06d6f5e034c2c9e8de56059220735
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-07-25 16:58:51 -0700

    feat: Finish colouring of buttons; Global CSS for UI components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './Button.scss';
+import "./Button.scss";
 
-import {
-  Command,
-  LucideIcon
-} from 'lucide-react';
+import { Command, LucideIcon } from "lucide-react";
 
 export interface ButtonProps {
   label: string;
   outlined: boolean;
   icon: LucideIcon;
-  color: 'primary' | 'red' | 'yellow' | 'green' | 'blue';
+  color: "primary" | "red" | "yellow" | "green" | "blue";
 }
 
 const Button = (props: ButtonProps) => {
   return (
-    <button className={props.outlined ? 'btn outlined' : 'btn'} data-color={props.color}>
+    <button
+      className={props.outlined ? "btn outlined" : "btn"}
+      data-color={props.color}
+    >
       <span className="text">{props.label}</span>
-      {props.icon ? <props.icon className="icon" size={16} /> : <Command className="icon" size={16} />}
+      {props.icon ? (
+        <props.icon className="icon" size={16} />
+      ) : (
+        <Command className="icon" size={16} />
+      )}
     </button>
   );
 };
